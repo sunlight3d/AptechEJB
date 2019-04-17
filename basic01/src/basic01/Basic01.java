1fdcc54bbccc900a20c97724a03e178eb696a948
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-17 14:57:32 +0700

    update

---
+++ basic01/src/basic01/Basic01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic01;
+
+import com.samples.Calculation;
+
+class Basic01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Calculation calculation = new Calculation();
+       calculation.doSomething();
+    }
+    
+}
