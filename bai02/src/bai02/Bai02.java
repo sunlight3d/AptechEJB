656f5133b47a3e6a591b70122ba12a0a3a3873e4
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-20 10:14:13 +0700

    update

---
+++ bai02/src/bai02/Bai02.java
@@ -0,0 +1,26 @@
+/*
+/Applications/XAMPP/bin/mysql -u root -p
+show databases;
+CREATE DATABASE ejbTutorials;
+USE ejbTutorials;
+CREATE TABLE books (
+   id     integer PRIMARY KEY,
+   name   varchar(50)
+);
+ */
+package bai02;
+
+/**
+ *
+ * @author hoangnd
+ */
+public class Bai02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
